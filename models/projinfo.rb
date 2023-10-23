32f7a20f3b66f60bd81b38b86f130a0ada80f93c
Linus Nilsson <linus.nilsson@uppmax.uu.se>
2023-10-23 13:32:39 +0200

    Remove commented code

--- models/projinfo.rb
+++ models/projinfo.rb
@@ -8,7 +8,6 @@ class Projinfo
   def main
     cmd = "/usr/local/bin/projinfo -r Alvis --json"
     data, stderr_str, status = Open3.capture3(cmd)
-    #data = File.read("/tmp/projinfo.json")
     projects = JSON.parse(data)
     target = Etc.getpwuid.name
     user_projects = projects.select do |project|
